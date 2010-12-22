db674ec1528c276b5d06a2b7acd6cc3feaf8f2ca
Marcel Dancak <marcel.dancak@gista.sk>
2010-12-22 17:34:02 +0100

    * added pre-commit script for git to automatically updating of application's version name. About windows shows this version name.

--- src/sk/gista/android/app/About.java
+++ src/sk/gista/android/app/About.java
@@ -2,7 +2,11 @@ package sk.gista.android.app;
 
 import sk.gista.android.maps.R;
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class About extends Activity {
 
@@ -10,5 +14,15 @@ public class About extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		String version;
+		try {
+			PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_META_DATA);
+			version = pInfo.versionName;
+		} catch (NameNotFoundException e) {
+			version = "Unknown";
+		}
+		
+		TextView versionView = (TextView) findViewById(R.id.about_version);
+		versionView.setText(" version: " + version);
 	}
 }
