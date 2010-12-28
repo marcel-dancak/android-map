79020b08a8673822d06ed047375e2c613a0b72d9
Marcel Dancak <marcel.dancak@gista.sk>
2010-12-28 12:49:26 +0100

    * sending network-debugging packets in special thread with the use of LinkedBlockingQueue

--- src/sk/gista/android/utils/NetworkDebugger.java
+++ src/sk/gista/android/utils/NetworkDebugger.java
@@ -6,7 +6,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
+import sk.gista.android.maps.Layer;
 import sk.gista.android.maps.Layer.Tile;
 
 public class NetworkDebugger {
@@ -26,17 +30,24 @@ public class NetworkDebugger {
 	
 	public static String server = "10.0.2.2";
 	public static int serverPort = 8090;
+	public static boolean debuggingEnabled = true;
 	
 	public static void sendProgress(Tile tile, int progress, int bytes) {
-		sendBinaryData(tile, Signal.PROGRESS_UPDATE, new int[] {progress, bytes});
+		if (debuggingEnabled) {
+			sendBinaryData(tile, Signal.PROGRESS_UPDATE, new int[] {progress, bytes});
+		}
 	}
 	
 	public static void sendFinished(Tile tile) {
-		sendBinaryData(tile, Signal.FINISHED, new int[0]);
+		if (debuggingEnabled) {
+			sendBinaryData(tile, Signal.FINISHED, new int[0]);
+		}
 	}
 	
 	public static void sendSignal(Tile tile, Signal signal) {
-		sendBinaryData(tile, signal, new int[0]);
+		if (debuggingEnabled) {
+			sendBinaryData(tile, signal, new int[0]);
+		}
 	}
 	
 	private static void sendBinaryData(Tile tile, Signal signal, int[] data) {
@@ -51,9 +62,13 @@ public class NetworkDebugger {
 				out.writeInt(i);
 			}
 			byte[] packetData = bos.toByteArray();
-			sendPacket(packetData, packetData.length);
+			sendPacket(packetData);
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {}
 		}
 	}
 	
@@ -67,20 +82,68 @@ public class NetworkDebugger {
 			out.writeInt(tile.getZoomLevel());
 			out.writeInt(bytes);
 			byte[] data = bos.toByteArray();
-			sendPacket(data, data.length);
+			sendPacket(data);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private static void sendPacket(byte[] data, int length) {
+	/*
+	private static void sendPacket(byte[] data) {
 		DatagramSocket socket;
 		try {
 			socket = new DatagramSocket();
-			DatagramPacket packet = new DatagramPacket(data, length, InetAddress.getByName(server), serverPort);
+			DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(server), serverPort);
 			socket.send(packet);
+			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	*/
+	
+	private static void sendPacket(byte[] data) {
+		sendingThread.addPacketToSend(data);
+	}
+	
+	private static NetThread sendingThread = new NetThread();
+	
+	public static void startSendingThread() {
+		sendingThread.start();
+	}
+	
+	public static void stopSendingThread() {
+		sendingThread.run = false;
+	}
+	
+	static class NetThread extends Thread {
+		
+		private BlockingQueue<byte[]> queue = new LinkedBlockingQueue<byte[]>(1000);
+		private boolean run = true;
+		
+		@Override
+		public void run() {
+			DatagramSocket socket;
+			try {
+				socket = new DatagramSocket();
+				while(run) {
+					try {
+						byte[] packetData = queue.poll(1000, TimeUnit.MILLISECONDS);
+						if (packetData != null) {
+							DatagramPacket packet = new DatagramPacket(packetData, packetData.length, InetAddress.getByName(server), serverPort);
+							socket.send(packet);
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public void addPacketToSend(byte[] data) {
+			queue.offer(data);
 		}
 	}
 }
